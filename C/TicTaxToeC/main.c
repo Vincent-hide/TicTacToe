a36db7403fc06aaa0958b48578e0962b4f6e2d2b
Vincent-hide <hidehiro.okayama@gmail.com>
2020-12-28 16:04:21 -0500

    prototype of human and ai move added. also draw detected

--- C/TicTaxToeC/main.c
+++ C/TicTaxToeC/main.c
@@ -45,19 +45,84 @@ void printBoard(const int *board) {
 };
 
 // Checks if there is still an empty spot
-int HasEmpty(const int *board) {
+int hasEmpty(const int *board) {
     int index = 0;
     for(index = 0; index < 9; index++) {
         if(board[ConvertTo25[index]] == EMPTY) {
-            return true;
+            return 1;
         }
     }
     return 0;
 }
 
-void MakeMove(int *board, const int sq, const side) {
+void makeMove(int *board, const int sq, const side) {
     board[sq] = side;
 
+}
+
+// for development stage: loop throgh all the calls. and store the idnex of the empty cell into availableMoves and randomly return the value stored in availableMoves.
+int getComputerMove(const int *board) {
+    int index = 0;
+    int numFree = 0; // the number of empty cell
+    int availableMoves[0];
+    int randMove = 0;
+
+    for(index = 0; index < 9; ++index) {
+        if(board[ConvertTo25[index]] == EMPTY) {
+            availableMoves[numFree++] = ConvertTo25[index];
+        }
+    }
+
+    randMove = (rand() % numFree);
+    return availableMoves[randMove];
+}
+
+int getHumanMove(const int *board) {
+    char userInput[4];
+
+    int moveOk = 0;
+    int move = -1;
+
+    while(moveOk == 0) {
+        printf("Please enter a move form 1 to 9:");
+        fgets(userInput, 3, stdin); // take the first 3 chars of the given string as an input
+        fflush(stdin); // clear up a buffer
+
+        // if the correct input is given, the strlen(userInput) returns 2. ex. 7\n
+        if(strlen(userInput) != 2) {
+            printf("Invalid strlen()\n");
+            continue;
+        }
+
+        // transfer userInput into move
+        if(sscanf(userInput, "%d", &move) != 1) {
+            move = -1;
+            printf("Invalid sscanf()\n");
+            continue;
+        }
+
+
+        if(move < 1 || move > 9) {
+            move = -1;
+            printf("Invalid range\n");
+            continue;
+        }
+
+        move--; // indexing
+
+
+
+        if(board[ConvertTo25[move]] != EMPTY) {
+            move = -1;
+            printf("The given cell number is not available\n");
+            continue;
+        }
+
+        moveOk = 1;
+    }
+
+    printf("Making move ... %d\n", (move + 1));
+    return ConvertTo25[move];
 }
 
 // Initiate a game and facilitate a game
@@ -73,12 +138,24 @@ void runGame() {
     while(!GameOver) {
         if(Side == NOUGHTS) {
             // player move
-
+            LastMoveMade = getHumanMove(&board[0]);
+            makeMove(&board[0], LastMoveMade, Side);
+            Side = CROSSES;
         } else {
             // AI move
+            LastMoveMade = getHumanMove(&board[0]);
+            makeMove(&board[0], LastMoveMade, Side);
+            Side = NOUGHTS;
             printBoard(&board[0]);
         }
+
+        if(!hasEmpty(board)) {
+            printf("Game Over!\n");
+            GameOver = 1;
+            printf("It's a draw");
+        }
     }
+
 }
 
 int main()
